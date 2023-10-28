e6c3e0a619f081518e2214614bdb332093c40c86
Christof Ruch <christof.ruch@gmx.de>
2023-10-28 14:08:06 +0200

    Add full tree verification to VPinfo by loading all pages and traversing the tree to make sure all page pointers are correct.

--- VPinfo.cc
+++ VPinfo.cc
@@ -4,6 +4,9 @@
 #include <Windows.h>
 #endif
 #include <stdlib.h>
+#include "Seite.hh"
+
+#include <vector>
 
 void usageMessage()
 {
@@ -28,4 +31,43 @@ int main(int argc, char **argv)
   VPBaum baum(*argv);
 
   baum.baumInfo();
+
+  // Lade Start Seite
+  std::vector<long> seiten;
+  long start = baum.info.startSeite;
+  seiten.push_back(start);
+
+  while (seiten.size() > 0) {
+	  long start = seiten.front();
+	  seiten.erase(seiten.begin());
+	  auto seite = Seite(baum.seitengroesse, baum.baum, start);
+	  int* kennung = (int*)seite.lesen(sizeof(int));
+	  int* anzahl = (int*)seite.lesen(sizeof(int));
+	  switch (*kennung) {
+	  case 1: {
+		  // Innerer Knoten
+		  float* minSigma = (float*)seite.lesen(sizeof(float));
+		  for (int d = 0; d < baum.info.dimension; d++) seite.lesen(sizeof(float)); // VP überlesen
+		  printf("Innere Seite an %d Anzahl %d\n", start, *anzahl);
+		  for (int b = 0; b < *anzahl; b++) {
+			  float* mu2 = (float*)seite.lesen(sizeof(float));
+			  long* neueseite = (long*)seite.lesen(sizeof(long));
+			  printf("Seite %f bei %d\n", *mu2, *neueseite);
+			  seiten.push_back(*neueseite);
+		  }
+
+		  break;
+	  }
+	  case 2: {
+		  // Blatt
+		  float* minSigma = (float*)seite.lesen(sizeof(float));
+		  printf("Blatt an %d Anzahl %d sigma %f\n", start, *anzahl, *minSigma);
+		  break;
+	  }
+	  default:
+		  printf("Unbekannte kennung %d", *kennung);
+		  exit(-1);
+	  }
+  }
+  
 }
